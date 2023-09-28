fdfbafafaed127c3b7b4565e0b04de5d82c2bf95
Pingvu <nhlpool27@gmail.com>
2023-09-28 12:55:07 +0200

    Add project files.

---
+++ kapitel 2 uppgift 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace kapitel2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad heter din favorit bok?");
+            String boknamn = Console.ReadLine();
+            Console.WriteLine(boknamn + " är ganska bra bok");
+        }
+    }
+}
